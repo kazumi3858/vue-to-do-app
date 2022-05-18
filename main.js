55662fd7142be29f8567d935553416e546d54a25
kazumi <kazumi121589@yahoo.co.jp>
2022-05-19 00:01:17 +0900

    Add form tags. Changed variable names.

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+import Vue from 'vue'
+
 const App = {
   data () {
     return {
       contents: [],
       newContent: null,
-      editAction: false
+      editMode: false
     }
   },
   mounted () {
@@ -17,30 +19,37 @@ const App = {
   },
   methods: {
     addContent () {
-      // 実際に何かしたことを入力する
+      console.log('1')
       if (!this.newContent) {
         return
       }
       const content = { text: this.newContent, editAction: false }
       this.contents.push(content)
-      //this.newContent = ''
+      this.newContent = ''
       this.saveContents()
     },
     removeContent (index) {
+      console.log('2')
       this.contents.splice(index, 1)
       this.saveContents()
     },
     saveContents () {
+      console.log('3')
       const jsonContents = JSON.stringify(this.contents)
       localStorage.setItem('contents', jsonContents)
     },
     changeEditMode (index) {
-      this.contents[index].editAction = !this.contents[index].editAction
+      console.log('4')
+      this.contents[index].editMode = !this.contents[index].editMode
     },
     saveUpdatedContents (index) {
+      console.log('5')
       this.changeEditMode(index)
       this.saveContents()
     }
+  },
+  computed: {
+
   }
 }
 
